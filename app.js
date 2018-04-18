bd81db5a632f7d9d9a0e0b0e54852c77a61e0e5f
Dmitry Gavrushko <gavrushko98@gmail.com>
2018-04-18 22:55:19 +0300

    cleaning all the folders

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const db = require('./server/utils/MenuUtils');
 const mongoose = require('mongoose');
-
+let methods = require('./server/utils/methods');
+const bodyParser = require("body-parser");
 
 const app = express();
-
+app.use('/static', express.static('public'));
+app.use(bodyParser.json());
 
 mongoose.connect(`mongodb://localhost:27017`);
 
-app.put('/', (req, res) => {
+
+app.post('/login',(req,res) =>{
+    let user = methods.login(req.body.login,req.body.password);
+    user ? res.send(user): res.send(404,"Incorrect login or password");
+});
+
+app.post('/downloadMenu', (req, res) => {
     db.addMenu()
         .then(answer => { 
             console.log(answer);
@@ -17,12 +25,12 @@ app.put('/', (req, res) => {
 
 })
 
-app.get('/', (req, res) => {
-    db.findMenu(new Date(2018, 8, 25))
+app.get('/getMenu', (req, res) => {
+    db.findMenu(new Date(2018, 6, 20))
         .then(answer => res.send(answer))
         .catch(err => console.log(err));
 })
 
 app.listen(3000, () => {
-    console.log(`Server woyy `);
+    console.log(`Server is running...`);
 })
