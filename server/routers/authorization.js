0d039f2370cb8688b5bd3d9934e8528e22bf0068
Dmitry Gavrushko <gavrushko98@gmail.com>
2018-05-03 18:48:42 +0300

    update method's upBalance(special for LIZA) and login(work with users db)

--- server/routers/authorization.js
+++ server/routers/authorization.js
@@ -1,9 +1,12 @@
 let router = require('express').Router();
 let methods = require('../utils/QueryMethods');
+let users = require('../utils/UsersUtils');
 
 router.post('/login',(req,res) =>{
-    let user = methods.login(req.body.login,req.body.password);
-    user ? res.status(200).send(user): res.status(404).send('Incorrect login or password!!!');
+    methods.login(req.body.login,req.body.password)
+        .then(user => user ? res.status(200).send(user) : res.status(404).send('Data not found!!!'))
+        .catch(err => res.status(404).send(err))
+
 });
 
 module.exports = router;
