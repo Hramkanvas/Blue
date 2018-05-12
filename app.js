acb854ab3ad8611938fe6ee4a8cacfdeeb996bf4
Alex Dernovskaya <alexzetto@gmail.com>
2018-05-12 12:00:11 +0300

    deleted word static in express static

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 const app = express();
-app.use('/static', express.static('public'));
+app.use('', express.static('public'));
 app.use(bodyParser.json());
 
 mongoose.connect(`mongodb://localhost:27017`, function (err) {
