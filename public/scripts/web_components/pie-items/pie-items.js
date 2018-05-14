34b93a9e0227f33ab1d7a123a35b4dd11e94185f
Aexey <aleksmalyavko@gmail.com>
2018-05-14 23:18:18 +0200

    created pie-items

---
+++ public/scripts/web_components/pie-items/pie-items.js
@@ -0,0 +1,135 @@
+(function () {
+
+    let menu = {
+        'id': 1,
+        'fromDate': "20.02.2018",
+        'menuInfo': {
+            "mon": {
+                "bread": {
+                    weight: 2,
+                    count: 1
+                },
+                "soup": {
+                    weight: 22,
+                    count: 1
+                },
+                "egg": {
+                    weight: 12,
+                    count: 1
+                }
+            },
+            "wen": {
+                "bread": {
+                    weight: 2,
+                    count: 1
+                },
+                "soup": {
+                    weight: 22,
+                    count: 1
+                },
+                "egg": {
+                    weight: 12,
+                    count: 1
+                }
+            },
+            "tr": {
+                "bread": {
+                    weight: 2,
+                    count: 1
+                },
+                "soup": {
+                    weight: 22,
+                    count: 1
+                },
+                "egg": {
+                    weight: 12,
+                    count: 1
+                }
+            },
+            "th": {
+                "bread": {
+                    weight: 2,
+                    count: 1
+                },
+                "soup": {
+                    weight: 22,
+                    count: 1
+                },
+                "egg": {
+                    weight: 12,
+                    count: 1
+                }
+            },
+            "fr": {
+                "bread": {
+                    weight: 2,
+                    count: 1
+                },
+                "soup": {
+                    weight: 22,
+                    count: 1
+                },
+                "egg": {
+                    weight: 12,
+                    count: 1
+                }
+            },
+            "sb": {
+                "bread": {
+                    weight: 2,
+                    count: 1
+                },
+                "soup": {
+                    weight: 22,
+                    count: 1
+                },
+                "egg": {
+                    weight: 12,
+                    count: 1
+                }
+            }
+        }
+    };
+
+    
+    let template1 = `
+        <style>
+            .items {
+                display: grid;
+                grid-template-columns: repeat(auto-fit, minmax(350px, 1fr));
+                grid-gap: 50px 30px;
+            }
+        </style>
+
+        <div class="items">
+
+        </div>
+    `;
+
+    class Items extends HTMLElement {
+        constructor() {
+            super(); 
+            this.attachShadow({ mode: 'open' }).innerHTML = template1;
+            this.addItems = this.addItems.bind(this);
+            this.place = this.shadowRoot.querySelector(".items");
+        }
+
+        connectedCallback() {
+            this.addItems(menu);
+        }
+
+        addItems(menu) {
+            for (let day in menu.menuInfo){
+                let item = document.createElement('item-menu');
+                item.setAttribute("data-day", day);
+                item.innerHTML = day;
+                this.place.appendChild(item);
+            }
+        }
+
+    }
+   
+    customElements.define('items-menu', Items);
+    
+})();
+
