5cd89b1ba55e9f1e5b05f999a7aab19fb5cc2100
Dmitry Gavrushko <gavrushko98@gmail.com>
2018-05-17 18:39:33 +0300

    fix autorization

--- server/routers/authorization.js
+++ server/routers/authorization.js
@@ -4,7 +4,7 @@ let users = require('../utils/UsersUtils');
 
 router.post('/login', (req, res) => {
     methods.login(req.body.login, req.body.password)
-        .then(user => {
+        .then((user) => {
             user ? res.status(200).send(user) : res.status(404).send('User not found')
         })
         .catch(err => res.status(404).send(err))
