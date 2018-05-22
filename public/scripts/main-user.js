d9fff90f5ae88175910daba56300429b607c25ad
Hramyka <kaatusgro@gmail.com>
2018-05-22 10:38:43 +0300

    pie-arrows now it's module, again

--- public/scripts/main-user.js
+++ public/scripts/main-user.js
@@ -4,5 +4,5 @@ import {pieMenuItem} from "./web_components/pie_menu_item/pie-menu-item.js";
 import {pieItems} from "./web_components/pie_items/pie-items.js";
 import {pieFooter} from "./web_components/pie_footer/pie-footer.js";
 import {pieGeneralInfo} from "./web_components/pie_general_info/pie-general-info.js";
-import "./web_components/pie_arrows/pie-arrows.js";
+import {pieArrows} from "./web_components/pie_arrows/pie-arrows.js";
 import {pieShowMainPart} from "./web_components/pie_show_main/pie-show-main.js";
